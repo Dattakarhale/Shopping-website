363778e3aee591e5458db4e4052faa100cd1448c
Datta_Karhale <106510206+Dattakarhale@users.noreply.github.com>
2024-03-10 12:12:56 +0530

    Add files via upload

---
+++ src/app/component/cart/cart.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss']
+})
+export class CartComponent implements OnInit {
+
+  public products : any = [];
+
+  public product1: any =[
+                         {sname:"avinash",rollno:3},
+                         
+                         {sname:"shardul", rollno:45},
+                         {sname:"anisha",rollno:90}
+                        ];
+  public grandTotal !: number;
+  constructor(private cartService : CartService) { }
+
+  ngOnInit(): void {
+    this.cartService.getProducts()
+    .subscribe(res=>{
+      this.products = res;
+      this.grandTotal = this.cartService.getTotalPrice();
+    })
+  }
+  removeItem(item: any){
+    this.cartService.removeCartItem(item);
+  }
+  emptycart(){
+    this.cartService.removeAllCart();
+  }
+
+}
